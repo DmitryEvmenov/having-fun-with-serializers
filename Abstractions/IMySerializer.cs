739c466a8d71a505e5d9d1be034230d375bd3419
Dmitry Evmenov <DmitryEvmenov@coherentsolutions.com>
2018-03-29 20:07:25 +0300

    Added initial structure

---
+++ Abstractions/IMySerializer.cs
@@ -0,0 +1,8 @@
+﻿namespace Abstractions
+{
+    public interface IMySerializer
+    {
+        void Serialize<TObj>(TObj data, string pathTo);
+        TObj Deserialize<TObj>(string pathFrom);
+    }
+}
---
+++ XML/MyXmlSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Serialization;
+using Abstractions;
+
+namespace XML
+{
+    public class MyXmlSerializer : IMySerializer
+    {
+        private readonly XmlSerializer Serializer;
+
+        public TObj Deserialize<TObj>(string pathFrom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize<TObj>(TObj data, string pathTo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
