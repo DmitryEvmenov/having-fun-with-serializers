c61d751655b020de4ca7f63876174c65be9591c1
Dmitry Evmenov <DmitryEvmenov@coherentsolutions.com>
2018-03-30 20:39:31 +0300

    Added BinaryAdapt

---
+++ BinaryAdapt/BinarySerializerAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Abstractions;
+using Binary;
+
+namespace BinaryAdapt
+{
+    public class BinarySerializerAdapter : IMySerializer, INamed
+    {
+        private readonly BinarySerializer _serializer = new BinarySerializer();
+
+        public void Serialize<TObj>(TObj data, string pathTo)
+            => _serializer.Serialize(data, pathTo);
+
+        public TObj Deserialize<TObj>(string pathFrom)
+            => (TObj)_serializer.Deserialize(pathFrom);
+
+        public string FriendlyName => "Binary";
+    }
+}
